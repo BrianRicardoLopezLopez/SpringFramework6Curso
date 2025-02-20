2edff650485b6cb8b16850d93c9876e3c107754f
BrianRicardoLopezLopez <159819606+BrianRicardoLopezLopez@users.noreply.github.com>
2025-02-20 09:57:41 -0600

    conexion con el apirest del backend

--- react-app/src/services/ProductService.js
+++ react-app/src/services/ProductService.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const initProducts = [
     {
         id: 1,
@@ -15,6 +17,56 @@ const initProducts = [
     }
 ];
 
+const baseUrl = 'http://localhost:8080/products';
+
 export const listProduct = () => {
     return initProducts;
+}
+
+export const findAll = async () => {
+    try {
+        const response = await axios.get(baseUrl)
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+    return null;
+
+}
+
+export const create = async ({ name, description, price }) => {
+    try {
+        const response = await axios.post(baseUrl, {
+            name,
+            description,
+            price
+        })
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+    return undefined;
+}
+
+
+export const update = async ({id, name, description, price }) => {
+    try {
+        const response = await axios.put(`${baseUrl}/${id}`, {
+            name,
+            description,
+            price
+        })
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+    return undefined;
+}
+
+export const remove = async (id) =>{
+    try {
+       await axios.delete(`${baseUrl}/${id}`);
+    } catch (error) {
+        console.log(error);
+    }
 }

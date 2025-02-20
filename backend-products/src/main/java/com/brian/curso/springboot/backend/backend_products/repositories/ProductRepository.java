7f94a49c365c520815046c72ed754debab61d0bd
BrianRicardoLopezLopez <159819606+BrianRicardoLopezLopez@users.noreply.github.com>
2025-02-20 09:53:59 -0600

    nuevas clases de Backend para hacer un restApi

--- backend-products/src/main/java/com/brian/curso/springboot/backend/backend_products/repositories/ProductRepository.java
+++ backend-products/src/main/java/com/brian/curso/springboot/backend/backend_products/repositories/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.brian.curso.springboot.backend.backend_products.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.brian.curso.springboot.backend.backend_products.entities.Product;
 
+@CrossOrigin(origins = "http://localhost:5173")
+@RepositoryRestResource(path = "products")
 public interface ProductRepository extends CrudRepository<Product, Long>{
 
 }
